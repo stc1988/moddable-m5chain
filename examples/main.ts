ecf59481cbcc29ae6dea03adeefc8f1e1b025a57
STC <stc1988@users.noreply.github.com>
2026-05-02 23:26:45 +0900

    moddable/issues/1603

--- examples/main.ts
+++ examples/main.ts
@@ -4,8 +4,8 @@ interface ModModule {
 	main(): Promise<void> | void;
 }
 
-if (Modules.has("basic")) {
-	const mod = Modules.importNow("basic") as ModModule;
+if (Modules.has("mod")) {
+	const mod = Modules.importNow("mod") as ModModule;
 	await mod.main();
 } else {
 	trace("No module found.\n");
